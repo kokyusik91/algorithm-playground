f863ab5083b53ef2be670c14ff1f7cf4e9333250
kokyusik91 <63444730+kokyusik91@users.noreply.github.com>
2022-10-13 22:18:37 +0900

    ⚡️ Algo!

--- insertion-sort.js
+++ insertion-sort.js
@@ -3,6 +3,15 @@ const array = [5, 23, 4, 27, 3, 99];
 console.log(insertionSort(array));
 
 function insertionSort(arr) {
-  for (let i = 1; i < array.length; i++) {}
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] < arr[j]) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+      console.log('End!!');
+    }
+  }
   return arr;
 }
